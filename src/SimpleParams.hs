f204937c5e6064e948f95a2295c3b2821090e108
Viktar Basharymau <viktar.basharymau@gmail.com>
2015-09-13 20:31:17 +0300

    Simplify simpleParamsToJson using Data.List.Split.split

--- src/SimpleParams.hs
+++ src/SimpleParams.hs
@@ -3,13 +3,14 @@
 module SimpleParams where
 
 import Data.List hiding (find)
-import Data.String.Utils
+import Data.List.Split
+import Data.String.Utils (endswith, replace)
 import Safe
 import Data.Char
 
 parseEmbedded :: String -> String
 parseEmbedded str =
-  case split "#" str of
+  case splitOn "#" str of
     [name, typ] ->
       if endswith "@" typ
         then "{\"name\":\"" ++ name ++ "\",\"type\":\"" ++ replace "@" "" typ ++ "\",\"last\":true}"
@@ -20,23 +21,14 @@ processPart :: String -> String
 processPart str =
   let symb = headDef ' ' str
   in if isDigit symb then str
-     else if symb == ','
-           || symb == '['
-           || symb == '{'
-           || symb == ']'
-           || symb == '}'
-           || symb == ':'
+     else if symb `elem` specialCharacters
              then str
           else if "#" `isInfixOf` str then parseEmbedded str
                else "\"" ++ str ++ "\""
 
 simpleParamsToJson :: String -> String
 simpleParamsToJson sparams =
-  let s =  (replace "," " , ")
-         . (replace "[" " [ ")
-         . (replace "{" " { ")
-         . (replace "]" " ] ")
-         . (replace "}" " } ")
-         . (replace ":" " : ")
-         $ sparams
-  in "{" ++  (concatMap processPart $ splitWs s) ++ "}"
+  "{" ++  (concatMap processPart $ split (dropBlanks $ oneOf specialCharacters) sparams) ++ "}"
+
+specialCharacters :: [Char]
+specialCharacters = ",:[]{}"
